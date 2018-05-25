1adb95268a6e70b3ca01918aedd4159947cc659a
pass3010e <807801282@qq.com>
2018-05-26 01:34:53 +0800

    naive inline

---
+++ src/SuffixOpNode.java
@@ -0,0 +1,28 @@
+package AST;
+
+import Entity.Entity;
+import FrontEnd.ASTVisitor;
+import IR.Operand.Operand;
+
+import java.util.HashMap;
+
+public class SuffixOpNode extends UnaryOpNode {
+
+    public SuffixOpNode(UnaryOp op, ExprNode expr) {
+
+        super(op, expr);
+    }
+
+    @Override public void accept(ASTVisitor visitor) {
+
+        visitor.visit(this);
+    }
+
+    @Override public ExprNode Inline(HashMap<Entity, Operand> inlineMap) {
+
+        SuffixOpNode node = new SuffixOpNode(this.operator, this.expr);
+        node.expr = expr.Inline(inlineMap);
+        return node;
+    }
+
+}
---
+++ src/UnaryOpNode.java
@@ -0,0 +1,67 @@
+package AST;
+
+import Entity.Entity;
+import FrontEnd.ASTVisitor;
+import IR.Operand.Operand;
+import Type.Type;
+
+import java.util.HashMap;
+
+public class UnaryOpNode extends ExprNode {
+
+    public enum UnaryOp {
+        PRE_INC, PRE_DEC, SUF_INC, SUF_DEC,
+        ADD, MINUS, L_NOT, B_NOT
+    }
+
+    protected UnaryOp operator;//private
+    protected Type type;
+    protected ExprNode expr;
+
+    public UnaryOpNode(UnaryOp op, ExprNode expr) {
+
+        this.operator = op;
+        this.expr = expr;
+    }
+
+    public UnaryOp operator() {
+
+        return operator;
+    }
+
+    @Override
+    public Type type() {
+
+        return expr.type();
+    }
+
+    public ExprNode expr() {
+
+        return expr;
+    }
+
+    public void setExpr(ExprNode expr) {
+
+        this.expr = expr;
+    }
+
+    @Override
+    public Location location() {
+
+        return expr.location();
+    }
+
+    @Override
+    public void accept(ASTVisitor visitor) {
+
+        visitor.visit(this);
+    }
+
+    @Override public ExprNode Inline(HashMap<Entity, Operand> inlineMap) {
+
+        UnaryOpNode node = new UnaryOpNode(this.operator, this.expr);
+        node.expr = expr.Inline(inlineMap);
+        return node;
+    }
+
+}
