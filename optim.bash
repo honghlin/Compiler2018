eed8c3a0d33a99c6ce614fa24a1824234450a758
pass3010e <807801282@qq.com>
2018-05-30 18:28:38 +0800

    Update optim.bash

--- optim.bash
+++ optim.bash
@@ -1,15 +1,15 @@
-# this script is called when the judge wants our compiler to compile a source file.
-# print the compiled source, i.e. asm code, directly to stdout.
-# don't print anything other to stdout.
-# if you would like to print some debug information, please go to stderr.
-# $1 is the path to the source file.
-# $2 is the path to the target file.
-
-set -e
 cd "$(dirname "$0")"
-export CCHK="java -classpath ./lib/antlr-4.7.1-complete.jar:./bin Main"
-cat > program.txt   # save everything in stdin to program.txt
+cat > test.mx
+java -classpath ./lib/antlr-4.7.1-complete.jar:./bin cn.kzoacn.Builder
 
-$CCHK
-
-cat result.nasm
+cat program.txt | grep "get" >hasget
+if diff empty hasget >null; then
+	nasm -felf64 result.nasm 
+	gcc -static result.o 
+	./a.out > ans.out
+	./inputbuilder < ans.out > tmp.c
+	./c2nasm.sh tmp.c 
+	cat tmp.c.nasm
+else
+	cat result.nasm 
+fi
