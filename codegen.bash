325648335c2f7c13e0cbdbcd857c3f5315f9a2d7
pass3010e <807801282@qq.com>
2018-05-30 14:20:08 +0800

    update

--- codegen.bash
+++ codegen.bash
@@ -1,6 +1,9 @@
 set -e
 cd "$(dirname "$0")"
+export CCHK="java -classpath ./lib/antlr-4.7.1-complete.jar:./bin Main"
 cat > program.txt
-java -classpath ./lib/antlr-4.7.1-complete.jar:./bin  Main
+
+$CCHK
+
 cat result.nasm
 
