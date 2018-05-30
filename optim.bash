1e898849f32f3d93fba6d36643f94903829d0a4f
pass3010e <807801282@qq.com>
2018-05-30 18:44:42 +0800

    Update optim.bash

--- optim.bash
+++ optim.bash
@@ -1,7 +1,9 @@
+set -e
 cd "$(dirname "$0")"
-cat > test.mx
-java -classpath ./lib/antlr-4.7.1-complete.jar:./bin cn.kzoacn.Builder
+export CCHK="java -classpath ./lib/antlr-4.7.1-complete.jar:./bin Main"
+cat > program.txt   # save everything in stdin to program.txt
 
+$CCHK
 cat program.txt | grep "get" >hasget
 if diff empty hasget >null; then
 	nasm -felf64 result.nasm 
