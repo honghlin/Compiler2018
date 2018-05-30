45ce1c5a566ff2a9698c1cd2c3a3232ea845b42c
pass3010e <807801282@qq.com>
2018-05-31 02:20:08 +0800

    update

---
+++ src/IR/Operand/IR.java
@@ -0,0 +1,36 @@
+package IR;
+
+import FrontEnd.AST;
+import Entity.*;
+import IR.Operand.GlobalAddr;
+//import Entity.FunctionEntity;
+//import Entity.VariableEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IR {
+
+    //private List<DefinitionNode> definitionNodes;
+    //private List<ClassEntity> classEntities;
+    //private List<FunctionEntity> functionEntities;
+    //private List<VariableEntity> variableEntities;
+
+    //public List<Function> functions = new ArrayList<>();
+    //public List<VarDec> globalVariables = new ArrayList<>();
+    public AST ast;
+    public List<VariableEntity> globalInitializer = new ArrayList<>();
+    public List<StringConstantEntity> stringConstants = new ArrayList<>();
+    public List<FunctionEntity> Record = new ArrayList<>();
+
+    public GlobalAddr add(StringConstantEntity t) {
+        stringConstants.add(t);
+        return new GlobalAddr("string__" + Integer.toString(stringConstants.size() - 1), true);
+    }
+
+    public AST ast() {
+
+        return ast;
+    }
+
+}
