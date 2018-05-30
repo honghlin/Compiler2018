c01c8f8cf7c0695f3cb3ddc878eb3af4231e4e36
pass3010e <807801282@qq.com>
2018-05-30 19:16:06 +0800

    Update optim.bash

--- optim.bash
+++ optim.bash
@@ -1,4 +1,4 @@
-set -e
+
 cd "$(dirname "$0")"
 export CCHK="java -classpath ./lib/antlr-4.7.1-complete.jar:./bin Main"
 cat > program.txt   # save everything in stdin to program.txt
@@ -10,7 +10,7 @@ if diff empty hasget >null; then
 	gcc -static result.o 
 	./a.out > ans.out
 	./inputbuilder < ans.out > tmp.c
-	./c2nasm.sh tmp.c 
+	./c2nasm.sh tmp.c >null
 	cat tmp.asm
 else
 	cat result.nasm 
