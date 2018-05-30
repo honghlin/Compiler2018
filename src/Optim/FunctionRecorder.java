ee0b0e54e4e641f0d96a362267355b2b55f2b335
pass3010e <807801282@qq.com>
2018-05-30 23:28:44 +0800

    update

---
+++ src/Optim/FunctionRecorder.java
@@ -0,0 +1,40 @@
+package Optim;
+
+import AST.*;
+import Entity.*;
+import FrontEnd.Visitor;
+import Type.*;
+
+public class FunctionRecorder extends Visitor {
+
+    FunctionEntity entity;
+    boolean f = true;
+
+    public FunctionRecorder(FunctionEntity entity) {
+
+        this.entity = entity;
+
+    }
+
+    public boolean check() {
+
+        visit(entity.body());
+        if(!(entity.type() instanceof IntType) || !(entity.type() instanceof BoolType)) return false;
+        if(entity.varList() == null || entity.varList().size() != 1) return false;
+        if(!(entity.varList().get(0).type() instanceof IntType)) return false;
+        return true;
+    }
+
+    @Override public void visit(VariableNode node) {
+
+        if(node.entity().isGlobal()) f = false;
+        return;
+    }
+
+    @Override public void visit(FuncallNode node) {
+
+        if(node.functionType().entity().name().equals(entity.name())) f = false;
+    }
+
+
+}
